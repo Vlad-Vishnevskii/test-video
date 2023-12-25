74405d9f09127ac44c87030aebb6cf1f61fda314
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2023-12-25 13:03:39 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <iframe
+        width='100%'
+        height='auto'
+        src={'https://www.youtube.com/embed/ScMzIvxBSi4?si=E79c-iQDCSD4Z3TM'}
+        frameborder="0"
+        //allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      /> 
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
